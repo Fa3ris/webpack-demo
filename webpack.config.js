0bf44ce3a19c6c2b72c819e0db253ccd64339bde
Fa3ris <Fa3ris@users.noreply.github.com>
2020-06-17 22:21:45 +0200

    style: format document

--- webpack.config.js
+++ webpack.config.js
@@ -3,44 +3,44 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/print.js',
     },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-      rules: [
-          {
-              test: /\.css$/,
-              use: [
-                  'style-loader',
-                  'css-loader',
-              ],
-          },
-          {
-              test: /\.(png|jpg|svg|gif)$/,
-              use: [
-                  'file-loader',
-              ],
-          },
-          {
-              test: /\.(woff|woff2|eot|ttf|otf)$/,
-              use: [
-                'file-loader',
-              ],
-          },
-      ],
-  },
-  plugins : [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin(
-        {
-            title: 'Output Management',
-        }
-    ),
-  ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin(
+            {
+                title: 'Output Management',
+            }
+        ),
+    ],
 };
