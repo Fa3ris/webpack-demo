3b3e973be141c8d178c1c2a0496c2eddeb823866
Fa3ris <Fa3ris@users.noreply.github.com>
2020-06-17 22:07:15 +0200

    feat: split output in two bundles

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import _ from 'lodash';
 import './style.css';
 import Lion from './lion.png';
 import homeComponent from './app/home/home.js';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -19,6 +21,11 @@ function component() {
    element.appendChild(icon);
 
    element.appendChild(homeComponent());
+
+   btn.innerHTML = 'Click me and check the console!';
+   btn.onclick = printMe;
+
+   element.appendChild(btn);
   
     return element;
   }
