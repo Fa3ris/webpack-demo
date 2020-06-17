bd762ea192cb97520521d084c9cd8c26eb4d8108
Fa3ris <Fa3ris@users.noreply.github.com>
2020-06-17 22:19:05 +0200

    feat: clean dist folder before build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -35,6 +36,7 @@ module.exports = {
       ],
   },
   plugins : [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin(
         {
             title: 'Output Management',
